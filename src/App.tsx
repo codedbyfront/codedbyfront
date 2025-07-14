f798f0df88763a477359076b654e97e017e6755c
codedbyfront <codedbyfront@gmail.com>
2025-07-14 12:12:49 +0330

    test2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 
 function App() {
+
     return (
         <>
-            <h1 className="text-3xl font-bold">
+            <h1 className="text-3xl font-bold text-red-900">
                 Hello world!
             </h1>
         </>
